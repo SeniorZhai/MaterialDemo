21a66e339dee702f29024e176ddb248d6ca31b7d
仔仔 <370985116@qq.com>
2015-05-11 17:03:06 +0800

    toolbar动画

--- app/src/main/java/io/github/seniorzhai/materialdemo/MainActivity.java
+++ app/src/main/java/io/github/seniorzhai/materialdemo/MainActivity.java
@@ -7,20 +7,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private DrawerLayout mDrawerLayout;
+    private View logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("");
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        logo = findViewById(R.id.ivLogo);
+        toolbar.setTitle("");
+        initDrawer();
+        if (savedInstanceState == null) {
+            startIntroAnimation();
+        }
+    }
+
+    private void initDrawer() {
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -28,6 +38,14 @@ public class MainActivity extends AppCompatActivity {
                 R.string.navigation_drawer_close);
         actionBarDrawerToggle.syncState();
         mDrawerLayout.setDrawerListener(actionBarDrawerToggle);
+    }
+
+    private void startIntroAnimation() {
+        int actionBarSize = Utils.dpToPx(56);
+        toolbar.setTranslationY(-actionBarSize);
+        logo.setTranslationY(-actionBarSize);
+        toolbar.animate().translationY(0).setDuration(300).setStartDelay(300);
+        logo.animate().translationY(0).setDuration(300).setStartDelay(450);
     }
 
     @Override
