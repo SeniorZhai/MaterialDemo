a2ea41d5877d3655a4acb3e893808034e9153e43
仔仔 <370985116@qq.com>
2015-05-11 18:17:51 +0800

    一部分图片资源，FAB动画

---
+++ app/src/main/java/io/github/seniorzhai/materialdemo/FeedAdapter.java
@@ -0,0 +1,65 @@
+package io.github.seniorzhai.materialdemo;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
+
+/**
+ * Created by zhaitao on 15/5/11.
+ */
+public class FeedAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private Context context;
+
+    public FeedAdapter(Context context) {
+        this.context = context;
+    }
+
+    // 创建View
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_feed, viewGroup, false);
+        CellFeedViewHolder cellFeedViewHolder = new CellFeedViewHolder(view);
+        return cellFeedViewHolder;
+    }
+
+    //数据与界面绑定
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+        runEnterAnimation(viewHolder.itemView, position);
+    }
+
+    // 数据的数量
+    @Override
+    public int getItemCount() {
+        return 100;
+    }
+
+    // 持有Item的每个元素
+    public static class CellFeedViewHolder extends RecyclerView.ViewHolder {
+        public CellFeedViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    private boolean animteItems = false;
+
+    private void runEnterAnimation(View view, int position) {
+        if (!animteItems) {
+            return;
+        }
+        view.setTranslationY(Utils.getScreenHeight(context));
+        view.animate()
+                .translationY(0)
+                .setInterpolator(new DecelerateInterpolator(3f))
+                .setDuration(700)
+                .start();
+    }
+
+    public void updateItems(boolean animated) {
+        animteItems = animated;
+        notifyDataSetChanged();
+    }
+}
--- app/src/main/java/io/github/seniorzhai/materialdemo/MainActivity.java
+++ app/src/main/java/io/github/seniorzhai/materialdemo/MainActivity.java
@@ -1,13 +1,18 @@
 package io.github.seniorzhai.materialdemo;
 
+import android.animation.Animator;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.OvershootInterpolator;
+import android.widget.ImageButton;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private DrawerLayout mDrawerLayout;
     private View logo;
+    private RecyclerView recyclerView;
+    ImageButton btnCreate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +29,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        recyclerView = (RecyclerView) findViewById(R.id.rvFeed);
         logo = findViewById(R.id.ivLogo);
+        btnCreate = (ImageButton) findViewById(R.id.btnCreate);
         toolbar.setTitle("");
         initDrawer();
+        initFeed();
         if (savedInstanceState == null) {
             startIntroAnimation();
+        } else {
+            feedAdapter.updateItems(false);
         }
     }
 
@@ -41,11 +53,52 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startIntroAnimation() {
+        btnCreate.setTranslationY(2 * getResources().getDimensionPixelOffset(R.dimen.btn_fab_size));
         int actionBarSize = Utils.dpToPx(56);
         toolbar.setTranslationY(-actionBarSize);
         logo.setTranslationY(-actionBarSize);
         toolbar.animate().translationY(0).setDuration(300).setStartDelay(300);
-        logo.animate().translationY(0).setDuration(300).setStartDelay(450);
+        logo.animate().translationY(0).setDuration(300).setStartDelay(450).setListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+//                feedAdapter.updateItems(true);
+                btnCreate.animate()
+                        .translationY(0)
+                        .setInterpolator(new OvershootInterpolator(1.f))
+                        .setStartDelay(300)
+                        .setDuration(400)
+                        .start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        });
+    }
+
+    private FeedAdapter feedAdapter;
+
+    private void initFeed() {
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this) {
+            @Override
+            protected int getExtraLayoutSpace(RecyclerView.State state) {
+                return 300;
+            }
+        };
+        recyclerView.setLayoutManager(linearLayoutManager);
+        feedAdapter = new FeedAdapter(this);
+        recyclerView.setAdapter(feedAdapter);
     }
 
     @Override
